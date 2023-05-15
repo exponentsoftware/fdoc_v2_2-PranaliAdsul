3c0ab295aa7b717a76e658a5f210e1d6e06ea6cc
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-15 20:52:18 +0530

    Problem 2.c Solved

--- 2.js
+++ 2.js
@@ -23,7 +23,7 @@
 // // Output: Updates the student with name 'Alice' to have age 20, new grades, and updated hobbies array
 // ```
 let students = [
-    {name : 'Alice' , age :20 , grades :[80] , hobbies : ['cricket','swimming']},
+    {name : 'Alex' , age :20 , grades :[80] , hobbies : ['cricket','swimming']},
     {name : 'Sanvi' , age :21 , grades : [88] , hobbies : ['travelling']},
     {name : 'Rohan' , age :20, grades : [72] , hobbies : ['singing']},
     {name : 'Jui' , age :21 , grades : [85], hobbies : ['dancing']},
@@ -31,12 +31,12 @@ let students = [
     {name : 'Mohan' , age :19 , grades : [96], hobbies : ['coding']},
     ]
     
-    // function findTopStudents(arr, marks){
-    //     let toppers = arr.filter(student => student.grades >= marks)
+    function findTopStudents(arr, marks){
+        let toppers = arr.filter(student => student.grades >= marks)
         
-    //     return toppers
-    //   }
-    //   console.log(findTopStudents(students, 85))
+        return toppers
+      }
+      console.log(findTopStudents(students, 85))
    
    
     function addHobby(arr, name, hobby){
@@ -50,4 +50,17 @@ let students = [
       }
       console.log(addHobby(students,"Sanvi", "bike-riding"))
        console.log(addHobby(students,"Mohan", "books-reading"))
-      
+     function updateStudent(arr, name, obj){
+      arr.forEach(student => {
+        if(student.name === name){
+          student .age = obj.age;
+          student .grades = obj.grades;
+          student .hobbies = obj.hobbies;
+        }
+      })
+      return arr
+    }
+    console.log(updateStudent(students, 'Alex', { age: 22, grades: [85], hobbies: ['cricket','swimming','acting'] }))
+    console.log(updateStudent(students, 'Rohan', { age: 23, grades: [77], hobbies: ['singing','playing guitar'] }))
+    
+    console.log('original', students)
